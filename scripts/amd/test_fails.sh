ab43e56a16d18abdb58a251f553bd0c0290f219a
Michael Melesse <micmelesse@gmail.com>
2021-03-29 14:50:44 -0500

    update test fail script

--- scripts/amd/test_fails.sh
+++ scripts/amd/test_fails.sh
@@ -2,13 +2,15 @@
 PWD=$(pwd)
 export PATH="${PATH}:${PWD}/third_party/kaldi/submodule/src/featbin/"
 export PATH="${PATH}:${PWD}/third_party/install/bin"
+export KALDI_ROOT=$PWD
 
+export PYTORCH_AUDIO_TEST_WITH_ROCM=1
 # FAILING UNIT TESTS
-# pytest -s test/torchaudio_unittest/backend/soundfile/save_test.py::TestFileObject::test_fileobj_flac
+pytest -s test/torchaudio_unittest/backend/soundfile/save_test.py::TestFileObject::test_fileobj_flac
 
 pytest test/torchaudio_unittest/functional/torchscript_consistency_cuda_test.py::TestFunctionalFloat32::test_griffinlim
-# pytest test/torchaudio_unittest/functional/torchscript_consistency_cuda_test.py::TestFunctionalFloat64::test_griffinlim
+pytest test/torchaudio_unittest/functional/torchscript_consistency_cuda_test.py::TestFunctionalFloat64::test_griffinlim
 
-# pytest test/torchaudio_unittest/transforms/torchscript_consistency_cuda_test.py::TestTransformsFloat32::test_GriffinLim
-# pytest test/torchaudio_unittest/transforms/torchscript_consistency_cuda_test.py::TestTransformsFloat64::test_GriffinLim
+pytest test/torchaudio_unittest/transforms/torchscript_consistency_cuda_test.py::TestTransformsFloat32::test_GriffinLim
+pytest test/torchaudio_unittest/transforms/torchscript_consistency_cuda_test.py::TestTransformsFloat64::test_GriffinLim
 
