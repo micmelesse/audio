37d4cc88e2f38353900d59daf6785507928fda9e
Michael Melesse <micmelesse@gmail.com>
2021-03-29 17:44:37 -0500

    add remote script

---
+++ scripts/amd/git_remote_add.sh
@@ -0,0 +1,3 @@
+git remote add upstream https://github.com/pytorch/audio
+git remote add rocm_fork https://github.com/ROCmSoftwarePlatform/audio
+git remote add private_fork https://github.com/micmelesse/audio
