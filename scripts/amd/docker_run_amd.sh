5b8fda0c2a63f5b550d413d99d0fb01b39d5fc6a
Michael Melesse <micmelesse@gmail.com>
2021-04-21 12:24:48 -0700

    update scripts

--- scripts/amd/docker_run_amd.sh
+++ scripts/amd/docker_run_amd.sh
@@ -3,17 +3,17 @@ alias drun_nodevice='sudo docker run -it --rm --network=host --ipc=host --shm-si
 
 VOLUMES="-v $HOME/dockerx:/dockerx -v /data:/data"
 
-WORK_DIR='-w /dockerx/audio'
-# WORK_DIR='-w /root/audio'
+# WORK_DIR='/dockerx/audio'
+WORK_DIR='/root/audio'
 
 # IMAGE_NAME=rocm/pytorch
 # IMAGE_NAME=rocm/pytorch:rocm4.0_ubuntu18.04_py3.6_pytorch
 # IMAGE_NAME=rocm/pytorch:rocm4.0.1_ubuntu18.04_py3.6_pytorch
 IMAGE_NAME=audio_amd
 
-CONTAINER_ID=$(drun --user root -d $WORK_DIR $VOLUMES $IMAGE_NAME)
+CONTAINER_ID=$(drun --user root -d -w $WORK_DIR $VOLUMES $IMAGE_NAME)
 echo "CONTAINER_ID: $CONTAINER_ID"
-docker cp . $CONTAINER_ID:/root/audio
+docker cp . $CONTAINER_ID:$WORK_DIR
 docker attach $CONTAINER_ID
 docker stop $CONTAINER_ID
 docker rm $CONTAINER_ID
--- scripts/amd/setup.sh
+++ scripts/amd/setup.sh
@@ -1,3 +1,3 @@
 pip3 install ninja typing pytest scipy numpy parameterized
 pip3 install -r requirements.txt 
-pip install scipy -U
+pip3 install scipy -U
